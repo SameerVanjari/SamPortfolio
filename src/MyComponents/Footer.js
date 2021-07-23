ba919f47b3d6346520d6a45f6bee611df335c493
Sameer <ssv6132@gmail.com>
2021-07-23 20:33:38 +0530

    another commit

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
     }
     return (
         <div className="text-center bg-dark py-4 text-white" style={styler}>
-            <Link to=""><p className="inline-block ">&copy;Sameer Portfolio</p></Link>
+            <Link to="/"><p className="inline-block ">&copy;Sameer Portfolio</p></Link>
             <p className="float-right text-gray-400"><strong>Email: </strong>ssv6132@gmail.com<br/><strong>Ph: </strong>+91 7768955586</p>
         </div>
     )
